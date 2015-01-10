a7529bc5b86c0ee715d692c7cc3b859ffca6166f
Conor Gilmer <conor.gilmer@gmail.com>
2015-01-10 10:48:35 +0000

    pelican add sm links

--- pelican2/settings.py
+++ pelican2/settings.py
@@ -6,6 +6,6 @@ SITENAME = u'Conors Musings'
 
 SITEURL = 'http://conorgilmer.github.io//pelican2/output'
 
-SOCIAL =(('twitter', 'http://twitter.com/conorgilmer'), ('github', 'http://conorgilmer.github.io'),('facebook', 'https://www.facebook.com/conorgilmer'),)
+SOCIAL =(('twitter', 'http://twitter.com/conorgilmer'), ('youtube', 'https://www.youtube.com/user/cjgilmer/videos'), ('linkedin', 'https://www.linkedin.com/in/conorgilmer'), ('github', 'http://conorgilmer.github.io'),('facebook', 'https://www.facebook.com/conorgilmer'),)
 
 TIMEZONE = "GMT"
