9c7119d1e5a82365b5e5a29c05064cabd054520c
Conor Gilmer <conor.gilmer@gmail.com>
2015-01-10 15:03:57 +0000

    added about me

--- pelican2/settings.py
+++ pelican2/settings.py
@@ -6,6 +6,6 @@ SITENAME = u'Conors Musings'
 
 SITEURL = 'http://conorgilmer.github.io//pelican2/output'
 
-SOCIAL =(('twitter', 'http://twitter.com/conorgilmer'), ('google-plus', 'https://plus.google.com/111664186016659524449'), ('youtube', 'https://www.youtube.com/user/cjgilmer/videos'), ('linkedin', 'https://www.linkedin.com/in/conorgilmer'), ('github', 'http://conorgilmer.github.io'),('facebook', 'https://www.facebook.com/conorgilmer'),)
+SOCIAL =(('twitter', 'http://twitter.com/conorgilmer'), ('google-plus', 'https://plus.google.com/111664186016659524449'), ('youtube', 'https://www.youtube.com/user/cjgilmer/videos'), ('aboutme', 'http://about.me/conor.gilmer'), ('linkedin', 'https://www.linkedin.com/in/conorgilmer'), ('github', 'http://conorgilmer.github.io'),('facebook', 'https://www.facebook.com/conorgilmer'),)
 
 TIMEZONE = "GMT"
