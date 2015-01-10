8f44d6281fc63c70fa2c3d701cc061338a5dae8d
Conor Gilmer <conor.gilmer@gmail.com>
2015-01-10 00:48:55 +0000

    pelican

---
+++ pelican2/content/dev/my-fav-lang.md
@@ -0,0 +1,10 @@
+Date: 2014-12-10
+Title: My Favourite Language
+
+##This is my favourite computer language
+
+* Item One
+* Item Two
+
+	This is some code
+
---
+++ pelican2/settings.py
@@ -0,0 +1,11 @@
+PATH = 'content'
+
+AUTHOR = u'Conor Gilmer'
+
+SITENAME = u'Conors Musings'
+
+SITEURL = 'http://conorgilmer.github.io//pelican2/output'
+
+SOCIAL =(('twitter', 'http://twitter.com/conorgilmer'), ('github', 'http://conorgilmer.github.io'),('facebook', 'https://www.facebook.com/conorgilmer'),)
+
+TIMEZONE = "GMT"
